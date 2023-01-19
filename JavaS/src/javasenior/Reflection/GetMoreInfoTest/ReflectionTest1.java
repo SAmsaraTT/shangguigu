80aa4eb7d22ef6dcffd437809ac0e5d823017f73
hliu <skulllht@gmail.com>
2023-01-18 19:28:53 -0500

    01/18/2023(2)

--- JavaS/src/javasenior/Reflection/GetMoreInfoTest/ReflectionTest1.java
+++ JavaS/src/javasenior/Reflection/GetMoreInfoTest/ReflectionTest1.java
@@ -1,7 +1,13 @@
 package javasenior.Reflection.GetMoreInfoTest;
+
+import org.junit.jupiter.api.Test;
+
 /*
 * 调用运行时类中指定的结构：属性，方法，构造器
 * */
 public class ReflectionTest1 {
+    @Test
+    public void test1() {
 
+    }
 }
